8942def4af2fb4f1e64421eaa07bf3c8254045cc
Eddy Marcos <emarcos@appvance.ai>
2024-03-23 20:40:29 -0600

    add iframe

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-$(document).ready(function () {
-  $("#myTable tbody tr").click(function () {
-    $(this).toggleClass("selected");
-  });
+function downloadSelectedRowsToExcel() {
+  var iframeContent = $("#appFrame").contents();
+  var selectedRows = iframeContent.find(".ag-row.selected");
 
-  $("#downloadBtn").click(function () {
-    const selectedRows = $("#myTable tbody tr.selected");
-    if (selectedRows.length === 0) {
-      alert("No rows selected!");
-      return;
-    }
+  var wb = XLSX.utils.book_new();
 
-    const wb = XLSX.utils.book_new();
-    selectedRows.each(function (index) {
-      const rowData = [];
-      $(this)
-        .find("td")
-        .each(function () {
-          rowData.push($(this).text());
-        });
-      const ws = XLSX.utils.aoa_to_sheet([rowData]);
-      XLSX.utils.book_append_sheet(wb, ws, "Sheet " + (index + 1));
+  selectedRows.each(function (index) {
+    var row = $(this);
+    var rowData = [];
+    row.find(".ag-cell").each(function () {
+      var cellData = $(this).text().trim();
+      rowData.push(cellData);
     });
 
-    const wbout = XLSX.write(wb, { type: "binary", bookType: "xlsx" });
-    function s2ab(s) {
-      const buf = new ArrayBuffer(s.length);
-      const view = new Uint8Array(buf);
-      for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
-      return buf;
-    }
-    saveAs(
-      new Blob([s2ab(wbout)], { type: "application/octet-stream" }),
-      "selected_rows.xlsx"
-    );
+    var ws = XLSX.utils.aoa_to_sheet([rowData]);
+    XLSX.utils.book_append_sheet(wb, ws, "Sheet" + (index + 1));
   });
+
+  var excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+  var blob = new Blob([excelBuffer], { type: "application/octet-stream" });
+  saveAs(blob, "selected_rows.xlsx");
+}
+
+$("#downloadBtn").on("click", function () {
+  downloadSelectedRowsToExcel();
+});
+
+$(document).ready(function () {
+  $("#appFrame")
+    .contents()
+    .find(".ag-row")
+    .on("click", function () {
+      $(this).toggleClass("selected");
+    });
 });
